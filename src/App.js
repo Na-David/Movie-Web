f0c8c9c642df964093e3dfd86b2630cfd6195f2a
David Na <hna206@naver.com>
2023-03-12 23:04:55 +1300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import Button from "./button";
 import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter(counter+1);
+
+  const iRun = () => console.log("I am Rendered");
+  useEffect(() => {
+    iRun();
+  },[])
   return (
     <div>
-      <h1 className={styles.title}>Weclom</h1>
-      <Button text={"Hyunmoon"}/>
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>hello</button>
+      <Button onClick={onClick} text={"Hyunmoon"}/>
     </div>
   );
 }
