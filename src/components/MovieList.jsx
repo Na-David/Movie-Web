e77482b99be04e6a9933b34f280e4c83665cb6d4
David Na <hna206@naver.com>
2023-04-02 22:32:01 +1200

    version changed

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
 import "./MovieList.css";
 
-const MovieList = ({id, img, title, genres, years, rating}) => {
+const MovieList = ({id, img, title, genres, years, rating, summary}) => {
     return (
         <div className="movie">
           <img src={img} alt = {title} className="movie_img"/>
@@ -11,13 +11,12 @@ const MovieList = ({id, img, title, genres, years, rating}) => {
               <Link to ={`/movie/${id}`}>{title}</Link>
             </h2>
             <h3 className="movie_year">{years}</h3>
-            {/* <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p> */}
+            <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
             <ul className="movie_detail">
               {genres.map(g => <li key={g}>{g}</li>)}
               <li className="movie_rating">rating : {rating}/10 </li>
             </ul>
           </div>
-
         </div>
     )
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -20,8 +20,8 @@ const Home = () => {
 
     return (
       <div>
-        <div className="slider">
-        <div className="slider_wrapper" >
+        <div className="container">
+        <div className="movies" >
           {movies.map((movie, idx) => (
             <div key = {idx} className = "slider_item">
               <MovieList
@@ -32,6 +32,7 @@ const Home = () => {
               title = {movie.title}
               genres = {movie.genres}
               rating = {movie.rating}
+              summary= {movie.summary}
                />
             </div>))}
         </div>
